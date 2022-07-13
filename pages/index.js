0e2f2f6e0abacd9e3c330b013593d63cd752c3db
briannelson95 <71141646+briannelson95@users.noreply.github.com>
2022-07-13 18:55:45 -0400

    basic ui structure

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
 
 export default function Home() {
   return (
-    <div>Hello world</div>
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className="text-2xl text-center">Which is better?</div>
+      <div className="p-2" />
+      <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
+        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="p-8">vs</div>
+        <div className="w-16 h-16 bg-red-200"></div>
+      </div>
+    </div>
   )
 }
