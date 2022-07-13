079a1dc6eedb1597bf20ed92eb99017c9894a0b2
briannelson95 <71141646+briannelson95@users.noreply.github.com>
2022-07-13 19:33:08 -0400

    add trpc

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,12 @@
+import { trpc } from "@/utils/trpc"
+
 
 export default function Home() {
+  const {data, isLoading } = trpc.useQuery(["hello", { text: 'brian'}])
+
+  if (isLoading) return <div>Loading...</div>
+
+  if (data) return <div>{data.greeting}</div>
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which is better?</div>
