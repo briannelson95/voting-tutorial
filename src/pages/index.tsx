68eafee6039ead5de24688f93daa79fe63340189
briannelson95 <71141646+briannelson95@users.noreply.github.com>
2022-07-13 21:35:10 -0400

    added random number generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,23 @@
+import { getOptionsForVote } from "@/utils/getRandom"
 import { trpc } from "@/utils/trpc"
+import { useEffect, useState } from 'react'
 
 
 export default function Home() {
+  // const [number, setNumber] = useState('')
+  // useEffect(() => setNumber('red'), [])
+
+  const [first, second] = getOptionsForVote();
+
+
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
-      <div className="text-2xl text-center">Which is better?</div>
+      <div className="text-2xl text-center">Who is your favorite?</div>
       <div className="p-2" />
       <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
-        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="w-16 h-16 bg-red-800">{first}</div>
         <div className="p-8">vs</div>
-        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="w-16 h-16 bg-red-800">{second}</div>
       </div>
     </div>
   )
---
+++ src/utils/getRandom.ts
@@ -0,0 +1,17 @@
+const MAX_LENGENDS = 21
+
+export const getRandom: (notThisOne?: number) => number = (
+    notThisOne
+) => {
+    const legendNumber = Math.floor(Math.random() * (MAX_LENGENDS - 1))
+
+    if (legendNumber != notThisOne) return legendNumber;
+    return getRandom(notThisOne)
+}
+
+export const getOptionsForVote = () => {
+    const firstId = getRandom()
+    const secondId = getRandom(firstId)
+
+    return [firstId, secondId];
+}
